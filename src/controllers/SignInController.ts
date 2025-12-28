38ac91e17797625f16b0f92f63d4461c48698952
Rhuan <rhuanfernandes.rf@gmail.com>
2025-12-28 10:56:57 -0300

    signin

--- src/controllers/SignInController.ts
+++ src/controllers/SignInController.ts
@@ -1,7 +1,11 @@
 
 import z from "zod";
 import type { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, ok } from "../utils/http";
+import { badRequest, ok, unauthorized } from "../utils/http";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+import { usersTable } from "../db/schema";
+import { compare } from "bcryptjs";
 
 const schema = z.object({
   email: z.email(),
@@ -21,9 +25,31 @@ export class SignInController {
       return badRequest({ errors: error.issues });
     }
 
+    // Verifica se o usuário existe
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        password: true,
+      },
+      where: eq(usersTable.email, data.email)
+    });
+
+    // Se não existir, retorna unauthorized
+    if (!user) {
+      return unauthorized({ error: 'Invalid credentials.' });
+    }
+
+    // Verifica se a senha está correta
+    const isPasswordValid = await compare(data.password, user.password);
+
+    // Se a senha estiver incorreta, retorna unauthorized
+    if (!isPasswordValid) {
+      return unauthorized({ error: 'Invalid credentials.' });
+    }
 
     return ok({
-      data,
+      user,
     });
   }
 }

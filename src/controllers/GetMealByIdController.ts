edad2f2295f67fc4171a68ed42752f6dbbfd5eea
Rhuan Lucas <rhuanfernandes.rf@gmail.com>
2026-01-10 15:20:47 -0300

    feat: adiciona endpoint para obter refeição por ID e implementa lógica correspondente

---
+++ src/controllers/GetMealByIdController.ts
@@ -0,0 +1,37 @@
+import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
+import z from "zod";
+import { db } from "../db";
+import { mealsTable } from "../db/schema";
+import { badRequest, created, ok } from "../utils/http";
+import { and, eq, gte, lte } from "drizzle-orm";
+
+const schema = z.object({
+  mealId: z.uuid(),
+})
+
+export class GetMealByIdController {
+  static async handle({ userId, params }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(params);
+
+    if (!success) {
+      return badRequest({ errors: error.issues })
+    }
+
+    const meal = await db.query.mealsTable.findFirst({
+      columns: {
+        id: true,
+        foods: true,
+        createdAt: true,
+        icon: true,
+        name: true,
+        status: true,
+      },
+      where: and(
+        eq(mealsTable.id, data.mealId),
+        eq(mealsTable.userId, userId),
+      )
+    });
+
+    return ok({ meal });
+  }
+}
